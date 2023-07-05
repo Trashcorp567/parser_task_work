a5ca0240bef4aba7fbad8462d80deb413428dca5
Trashcorp567 <Trashcorp567@gmail.com>
2023-07-05 06:35:01 +0300

    Final

--- src/fun.py
+++ src/fun.py
@@ -5,12 +5,18 @@ import os
 
 
 class VacancyAPI(ABC):
+    """
+    Абстрактный класс обязывающий реализовать метод API запросов
+    """
     @abstractmethod
     def get_vacancies(self):
         pass
 
 
 class VacancySaver(ABC):
+    """
+    Абстрактный класс обязывающий реализовать методы записи json
+    """
     @abstractmethod
     def save_vacancy(self):
         pass
@@ -29,62 +35,240 @@ class VacancySaver(ABC):
 
 
 class HeadHunterAPI(VacancyAPI):
-    @staticmethod
-    def get_vacancies():
-        response = requests.get('https://api.hh.ru/vacancies')
+    """
+    Класс для работы с API запросами сайта HeadHunter
+    """
+
+    @classmethod
+    def get_vacancies(cls, per_page=50, page=1, text=None):
+        """
+        Метод для фильтрации полученных данных
+        """
+        all_vacancies = []
+        headers = {
+            'User-Agent': 'Homework'
+        }
+        params = {
+            "text": text,
+            "page": page,
+            "per_page": per_page
+        }
+        response = requests.get('https://api.hh.ru/vacancies', headers=headers, params=params)
         if response.status_code == 200:
             vacancies = response.json()
-            return vacancies
+            all_vacancies.extend(vacancies.get('items', []))
         else:
             print(response.status_code)
 
+        return {'items': all_vacancies}
 
-class JSONVacansy(VacancySaver, HeadHunterAPI):
-    @staticmethod
-    def save_vacancy():
-        with open('vacancies.json', 'w') as file:
-            json.dump(HeadHunterAPI.get_vacancies(), file, indent=4)
 
-    @staticmethod
-    def update_vacancy():
-        JSONVacansy.delete_vacancy()
-        JSONVacansy.save_vacancy()
+class SuperJobAPI(VacancyAPI):
+    """
+    Класс для работы с API запросами сайта SuperJob
+    """
+    api_key = "v3.r.137657357.791fa7b6a4bbec4e3bb7bdd5dbf4214e9ceaace3.493002b668a537f2ec0eead1216ef5959c90e56a"
+    base_url = "https://api.superjob.ru/2.0"
 
     @staticmethod
-    def delete_vacancy():
+    def get_vacancies(page=0, count=20, keyword=None):
+        """
+        Метод для фильтрации полученных данных
+        Параметры запроса меняются в самом блоке интеракция с пользователем
+        """
+        url = f"{SuperJobAPI.base_url}/vacancies/"
+        headers = {
+            "X-Api-App-Id": SuperJobAPI.api_key
+        }
+        params = {
+            "keyword": keyword,
+            "page": page,
+            "count": count
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            vacancies = data.get("objects", [])
+            return vacancies
+        else:
+            print(data)
+
+        return []
+
+
+class Vacancy:
+
+    def __init__(self, name, link, salary, description, currency):
+        """
+        Инициализация класса по атрибутам полученным с помощьб метода from_json
+        """
+        self.name = name
+        self.link = link
+        self.salary = salary
+        self.description = description
+        self.currency = currency
+
+    @classmethod
+    def from_json(cls, data):
+        """
+        Метод для получения атрибутов из json файла для работы с HH и SJ
+        """
+        vacancies = []
+        try:
+            for vacancy_data in data["items"]:
+                name = vacancy_data.get('name')
+                link = vacancy_data.get('alternate_url')
+                if vacancy_data['salary'] != 0 or None:
+
+                    try:
+                        salary = vacancy_data['salary'].get('from')
+                        currency = vacancy_data['salary'].get('currency')
+
+                    except:
+                        salary = "Информация об оплате отсутствует"
+                        currency = ""
+
+                else:
+                    salary = "Информация об оплате отсутствует"
+                    currency = ""
+
+                description = vacancy_data['snippet'].get('responsibility')
+                vacancy = cls(name, link, salary, description, currency)
+                vacancies.append(vacancy)
+                return vacancies
+
+        except:
+            for vacancy_data in data:
+                name = vacancy_data["profession"]
+                link = vacancy_data['link']
+                if vacancy_data['payment_from'] == 0:
+                    salary == ""
+                    currency = ""
+                else:
+                    currency = vacancy_data["currency"]
+                    salary = vacancy_data['payment_from']
+                description = vacancy_data["work"]
+                vacancy = cls(name, link, salary, description, currency)
+                vacancies.append(vacancy)
+            return vacancies
+
+    def __str__(self):
+        return f"{self.name} ({self.salary})\n{self.description}\nLink: {self.link}"
+
+    def __repr__(self):
+        return f"Vacancy('{self.name}', '{self.link}', {self.salary}, '{self.description}')"
+
+    def __lt__(self, other):
+        return self.salary < other.salary
+
+    def __le__(self, other):
+        return self.salary <= other.salary
+
+    def __eq__(self, other):
+        return self.salary == other.salary
+
+    def __ne__(self, other):
+        return self.salary != other.salary
+
+    def __gt__(self, other):
+        return self.salary > other.salary
+
+    def __ge__(self, other):
+        return self.salary >= other.salary
+
+
+class JSONVacancy(VacancySaver, HeadHunterAPI, SuperJobAPI):
+    """
+    Методы редактирования полученных данных в JSON файл
+    При желании можно оъеденить, чтобы данные сохранялись в один файл
+    """
+    @classmethod
+    def save_vacancy(cls, vacancies):
+        with open('vacancies.json', 'w', encoding='utf-8') as file:
+            json.dump(vacancies, file, indent=4, ensure_ascii=False)
+
+    @classmethod
+    def update_vacancy(cls):
+        JSONVacancy.delete_vacancy()
+        JSONVacancy.save_vacancy()
+
+    @classmethod
+    def delete_vacancy(cls):
         os.remove('vacancies.json')
 
-    @staticmethod
-    def get_vacancy():
-        with open('vacancies.json', 'r') as file:
-            return file.read()
+    @classmethod
+    def get_vacancy(cls):
+        with open('vacancies.json', 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            vacancies = Vacancy.from_json(json_data)
+            return vacancies
+
+    @classmethod
+    def save_vacancy_for_sj(cls, vacancies):
+        with open('vacancies_sj.json', 'w', encoding='utf-8') as file:
+            json.dump(vacancies, file, indent=4, ensure_ascii=False)
+
+    @classmethod
+    def delete_vacancy_for_sj(cls):
+        os.remove('vacancies_sj.json')
+
+    @classmethod
+    def get_vacancy_sj(cls):
+        with open('vacancies_sj.json', 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            vacancies = Vacancy.from_json(json_data)
+            return vacancies
 
 
-vacancy_data = JSONVacansy.get_vacancy()
-vacancy_json = json.loads(vacancy_data)
-test_inp = str(input())
-for vacancy in vacancy_json['items']:
-    if test_inp in vacancy['name']:
-        print(vacancy['name'])
+def interact():
+    """
+    Функция работы с пользователем
+    """
+    # Взаимодействие с пользователем через консоль
+    print("Поиск вакансий:")
+    # Выбор платформы
+    platform_choice = input("Выбирете платформу для поиска(HH или SJ): ").upper()
+    if platform_choice == "HH":
+        text = input("Введите текст поискового запроса: ")
+        # Получение вакансий с использованием введенных пользователем данных
+        hh_api = HeadHunterAPI.get_vacancies(text=text)
+        # Проверка наличия вакансий
+        if hh_api and 'items' in hh_api:
+            filtered_vacancies = [item for item in hh_api['items'] if text.lower() in item['name'].lower()]
+            if filtered_vacancies:
+                JSONVacancy.save_vacancy(filtered_vacancies)
+
+                print(f"Найдено {len(filtered_vacancies)} вакансий:\n")
+
+                for vac in filtered_vacancies:
+                    vacancy = Vacancy.from_json({"items": [vac]})
+                    for vacancy_item in vacancy:
+                        print(f"{vacancy_item.name} -  {vacancy_item.salary} {vacancy_item.currency}")
+                        print(f"Ссылка на вакансию: {vacancy_item.link}\n")
+            else:
+                print("Вакансии по заданному запросу не найдены.")
+
+    elif platform_choice == "SJ":
+        user_choice = input("Введите наименование вакансии: ")
+        count = int(input("Введите количество вакансий для просмотра: "))
+        # Получение вакансий с использованием введенных пользователем данных
+        super_job_api = SuperJobAPI.get_vacancies(page=0, count=count, keyword=user_choice)
+        if super_job_api:
+            JSONVacancy.save_vacancy_for_sj(super_job_api)
+            # Инициализациия атрибутами класс Vacancy и присвоение их объекту
+            vacancies = Vacancy.from_json(super_job_api)
+            print(f"Найдено {len(vacancies)} вакансий:\n")
+            if vacancies:
+                for vac in vacancies:
+                    # Выводимые параметры можно изменить другими атрибутами класса
+                    print(f"Название: {vac.name}")
+                    print(f"Оплата: от {vac.salary} {vac.currency}")
+                    print(f"Ссылка на вакансию: {vac.link}\n")
+
     else:
-        print(f'Нет такого запроса {test_inp}')
-for vacancy in vacancy_json['items']:
-    print('Название:', vacancy['name'])
-    #print('URL:', vacancy["employer"]['name'])
-    #print(f"Сумма: {vacancy['salary']['from']} - {vacancy['salary']['to']}")
-    #print("")
-
-# Нужно додумать как получать фильтрованные данные, дописать класс и методы
-# для хранения информация именно тех проф которые подходят пользователю
-# Функции для сортировки вакансий(по названию по зп по требованиям(возможно)
-# Создать класс для работы с вакансиями.
-# В этом классе самостоятельно определить атрибуты, такие как название вакансии, ссылка на вакансию, зарплата,
-# краткое описание или требования и т.п. (не менее четырех) Класс должен поддерживать методы сравнения вакансий
-# между собой по зарплате и валидировать данные,
-# которыми инициализируются его атрибуты.
-# Определить абстрактный класс, который обязывает реализовать методы для добавления
-# Пока сделал только для одного hh
-# получить вакансии в отсортированном виде, получить вакансии,
-# в описании которых есть определенные ключевые слова, например "postgres"(????) и т.п.
-# Понять что я делаю
-# Дописать readme
+        # Сообщает о неправильном выборе платформы и удаляет словари, если они есть
+        JSONVacancy.delete_vacancy()
+        JSONVacancy.delete_vacancy_for_sj()
+        print("Платформа не найдена")
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.fun import interact
+
+if __name__ == '__main__':
+    interact()
